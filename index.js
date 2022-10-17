817a980eee18d0d6bbb25ee4b3bd109ca34f44d1
Truongminhdat <truongminhdat789@gmail.com>
2022-10-17 14:41:10 +0700

    update

---
+++ index.js
@@ -0,0 +1,19 @@
+var lengthOfLongestSubstring = function (s) {
+  var k = 0;
+  var maxLength = 0;
+  for (i = 0; i < s.length; i++) {
+    for (j = k; j < i; j++) {
+      if (s[i] === s[j]) {
+        k = j + 1;
+        break;
+      }
+    }
+    if (i - k + 1 > maxLength) {
+      maxLength = i - k + 1;
+    }
+  }
+  return maxLength;
+};
+
+var res = lengthOfLongestSubstring("abcabcbb");
+console.log("check length", res);
